7064bf7f475b8f80cae1d6c3dbc6925cbfa0d7a1
Itsmynool <124412539+Itsmynool@users.noreply.github.com>
2024-03-09 22:10:53 -0500

    .

--- AES-CFB.py
+++ AES-CFB.py
@@ -1,6 +1,7 @@
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.backends import default_backend
 import os
+import time
 
 def encrypt_file(file_path, key):
     with open(file_path, 'rb') as file:
@@ -17,11 +18,22 @@ def encrypt_file(file_path, key):
         encrypted_file.write(iv + ciphertext)
 
     print("File encrypted successfully!")
+    return encrypted_file_path
 
 def main():
     key = b'my_secret_key123'  # La clave debe tener 16, 24, o 32 bytes
     file_path = 'mensaje.txt'  # Asegúrate de tener este archivo en la misma ruta
-    encrypt_file(file_path, key)
+    encrypted_file_path = None
+
+    while encrypted_file_path is None:
+        try:
+            encrypted_file_path = encrypt_file(file_path, key)
+        except Exception as e:
+            print("Error:", e)
+            print("Retrying in 1 second...")
+            time.sleep(1)
+
+    print("Encryption completed!")
 
 if __name__ == "__main__":
     main()
--- reconocimiento_rostro.py
+++ reconocimiento_rostro.py
@@ -1,4 +1,6 @@
 import cv2
+import time
+import Desencriptado
 
 def load_names():
     names = {}
@@ -7,61 +9,118 @@ def load_names():
             names[idx] = line.strip()
     return names
 
-def recognize_faces():
-    # Cargamos el clasificador de reconocimiento facial de OpenCV
+def load_face_recognition_model():
     face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
     face_recognizer = cv2.face.LBPHFaceRecognizer_create()
     face_recognizer.read('trained_model.yml')
 
+    return face_cascade, face_recognizer
+
+def recognize_faces():
+    # Cargamos el clasificador de reconocimiento facial de OpenCV
+    face_cascade, face_recognizer = load_face_recognition_model()
     # Cargamos el mapeo de nombres
     names = load_names()
-
     # Definimos un umbral de confianza
     confidence_threshold = 60
-
     # Iniciamos la captura de video desde la cámara
     cap = cv2.VideoCapture(0)
+
+    recognized_time = None
+    unrecognized_time = None
+    recognized_person = None
+    undetected_face = None
+
+    access = False
 
     while True:
         # Capturamos un frame
         ret, frame = cap.read()
-
         # Volteamos horizontalmente el frame para aplicar el efecto espejo
         frame = cv2.flip(frame, 1)
-
         # Convertimos el frame a escala de grises
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
         # Detectamos caras en el frame
         faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
+        #print('FACES: ', faces)
+        #print('Names: ', names)
+        # Si hay al menos una cara detectada
+        if len(faces) > 0:
+            # Para cada cara detectada
+            for (x, y, w, h) in faces:
+                # Realizamos el reconocimiento facial
+                label, confidence = face_recognizer.predict(gray[y:y+h, x:x+w])
+                # Si la confianza es alta y la etiqueta está en el diccionario de nombres
+                if label in names and confidence < confidence_threshold:
+                    recognized_person = names[label]
+                    if recognized_time is None:
+                        if unrecognized_time is not None:
+                            unrecognized_time = None
+                        if undetected_face is not None:
+                            undetected_face = None
+                        recognized_time = time.time()  # Iniciar el contador si no ha comenzado
 
-        # Para cada cara detectada
-        for (x, y, w, h) in faces:
-            # Realizamos el reconocimiento facial
-            label, confidence = face_recognizer.predict(gray[y:y+h, x:x+w])
+                else:
+                    recognized_person = "Desconocido"
+                    if unrecognized_time is None:
+                        if recognized_time is not None:
+                            recognized_time = None
+                        if undetected_face is not None:
+                            undetected_face = None
+                        unrecognized_time = time.time()
 
-            # Si la confianza es alta y la etiqueta está en el diccionario de nombres
-            if label in names and confidence < confidence_threshold:
-                name = names[label]
-            else:
-                name = 'Desconocido'
-            
-            # Mostramos el nombre de la persona
-            cv2.putText(frame, name, (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36,255,12), 2)
-            
-            # Dibujamos un rectángulo alrededor de la cara
-            cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+                # Dibujar un rectángulo alrededor de la cara
+                cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+                # Mostrar el nombre o "Desconocido" en el cuadro de la persona detectada
+                cv2.putText(frame, recognized_person, (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36,255,12), 2)
 
-        # Mostramos el frame
+        # Mostrar el frame
         cv2.imshow('Face Recognition', frame)
 
-        # Salimos del bucle si se presiona la tecla 'q'
+        #print('TIEMPO RECONOCIDO: ', recognized_time)
+        #print('TIEMPO NO RECONOCIDO: ', unrecognized_time)
+
+        #print('RECOGNIZED TIME: ', recognized_time)
+        #print('UNRECOGNIZED TIME: ', unrecognized_time)
+        #and ((recognized_time is not None) or (unrecognized_time is not None))
+        if recognized_time is None and unrecognized_time is None and undetected_face is None:
+            undetected_face = time.time()
+
+        if recognized_time is not None:
+            recognized_time_mapping = time.time() - recognized_time
+            #print('RECOGNIZED: ', recognized_time_mapping)
+            if recognized_time_mapping >= 3:
+                print('Access grantied')
+                access = True
+                break
+        elif unrecognized_time is not None:
+            unrecognized_time_mapping = time.time() - unrecognized_time
+            #print('UNRECOGNIZED: ', unrecognized_time_mapping)
+            if unrecognized_time_mapping >= 3:
+                print('Access denied')
+                break
+        elif undetected_face is not None:
+            undetected_face_mapping = time.time() - undetected_face
+            #print('TIME OUT: ', undetected_face_mapping)
+            if undetected_face_mapping >= 5:
+                print('Timeout')
+                break
+
+        # Si no se detecta ningún rostro, reiniciar el contador
+        if len(faces) == 0:
+            recognized_time = None
+            unrecognized_time = None
+
+        # Salir del bucle si se presiona la tecla 'q'
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
-    # Liberamos la captura de video y cerramos todas las ventanas
+    # Liberar la captura de video y cerrar todas las ventanas
     cap.release()
     cv2.destroyAllWindows()
 
-# Realizamos el reconocimiento facial
+    if access:
+        Desencriptado.main()
+
+# Realizar el reconocimiento facial
 recognize_faces()
